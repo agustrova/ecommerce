1eff9ecbd68fedd77f5dc054bb55a79abd90421a
agustrova <agustinatrova@gmail.com>
2023-11-16 12:43:58 -0300

    Listo pintar productos desde js

---
+++ js/products.js
@@ -0,0 +1,29 @@
+const productsArray = JSON.parse(localStorage.getItem("products"))
+const cardContainer = document.querySelector(".card-container")
+
+
+function pintarProductos(arrayPintar) {
+    cardContainer.innerHTML = ""
+    arrayPintar.forEach ((prod, index) => {
+    cardContainer.innerHTML += 
+` <div class="card">
+    <header class="cass-header">
+            <img class="card-img" src=${prod.img} alt="">
+    </header>
+    <div class="card-body">
+        <p class="card-name">${prod.fullname}</p>
+        <p class="card-description">${prod.description}</p>
+            <div class="card-values">
+                <div class="card-oldprice">${prod.oldprice}</div>
+                <div class="card-newprice">${prod.newprice}</div>
+            </div>    
+    </div>
+    <footer class="card-footer">
+        <button class="card-btn">Agregar al carrito</button>
+    </footer>
+</div>`
+})
+}
+
+pintarProductos(productsArray)
+
